5da719ef081b4d3a1d0dd3ebba2593e7b2ad8684
Philipp Frenzel <philipp@frenzel.net>
2015-09-09 23:44:47 +0200

    enhance the activity form

--- views/widgets/views/_form.php
+++ views/widgets/views/_form.php
@@ -11,6 +11,20 @@ use kartik\datetime\DateTimePicker;
 use kartik\form\ActiveForm;
 use kartik\widgets\Select2;
 
+/**
+ * this js script allows people to press ctrl+s to save values
+ * @var [type]
+ */
+$script = <<<SKRIPT
+
+$('#activity-text').focus(function() {
+    $('#container_activity_input').show( 1000 );
+});
+
+SKRIPT;
+
+$this->registerJs($script);
+
 ?>
 
 <?php $form = ActiveForm::begin([
@@ -24,8 +38,21 @@ use kartik\widgets\Select2;
     ]
 ) ?>
 
-<div class="row" data-activity="form-group">
-    <div class="col-sm-12"><?= \Yii::t('net_frenzel_activity','Now'); ?>:
+<div class="form-group" data-activity="form-group">
+    <div class="col-sm-12">
+    <label class="control-label" for="activity-text"><?= \Yii::t('net_frenzel_activity','Notes'); ?></label>
+        <?= net\frenzel\textareaautosize\yii2textareaautosize::widget([
+              'model'=> $model,
+              'attribute' => 'text'
+          ]);
+        ?>
+        <?= Html::error($model, 'text', ['data-activity' => 'form-summary', 'class' => 'help-block hidden']) ?>
+    </div>
+</div>
+
+<div id="container_activity_input" style="display:none">
+    <div class="col-md-12" data-activity="form-group">
+        <label class="control-label" for="activity-text"><?= \Yii::t('net_frenzel_activity','Now'); ?></label>        
         <?= $form->field($model, 'type')->radioButtonGroup($model->TypeArray,[
                 //'class' => 'btn-group-sm',
                 'id' => 'type-create',
@@ -33,69 +60,58 @@ use kartik\widgets\Select2;
             ])->label(false);?>
          <?= Html::error($model, 'type', ['data-activity' => 'form-summary', 'class' => 'help-block hidden']) ?>
     </div>
-</div>
 
-<div class="form-group" data-activity="form-group">
-    <div class="col-sm-12"><?= \Yii::t('net_frenzel_activity','Notes'); ?>:
-        <?= net\frenzel\textareaautosize\yii2textareaautosize::widget([
-		      'model'=> $model,
-		      'attribute' => 'text'
-		  ]);
-    	?>
-        <?= Html::error($model, 'text', ['data-activity' => 'form-summary', 'class' => 'help-block hidden']) ?>
-    </div>
-</div>
-
-<div class="form-group" data-activity="form-group">
-        <div class="col-sm-4">
-            <?= \Yii::t('net_frenzel_activity','When'); ?>:
-            <?= DateTimePicker::widget([
-                'model' => $model,
-                'attribute' => 'next_at',
-                'type' => DateTimePicker::TYPE_COMPONENT_PREPEND,
-                'pluginOptions' => [
-                    'autoclose'=>true,
-                ]
-            ]); ?>
-        <?= Html::error($model, 'next_at', ['data-activity' => 'form-summary', 'class' => 'help-block hidden']) ?>
-    </div>
-    <div class="col-sm-8"><?= \Yii::t('net_frenzel_activity','Next'); ?>:
-        <?= $form->field($model, 'next_type')->radioButtonGroup($model->NextTypeArray,[
-                //'class' => 'btn-group-sm',
-                'id' => 'next_type-create',
-                'itemOptions' => ['labelOptions' => ['class' => 'btn btn-primary btn-sm']]
-            ])->label(false);?>
-        <?= Html::error($model, 'next_type', ['data-activity' => 'form-summary', 'class' => 'help-block hidden']) ?>
-    </div>    
-</div>
-
-<div class="row">
-    <div class="col-md-12">
-        <div class="col-md-9">
-            <?php
-            $userDesc = empty($model->next_by) ? '' : $model->responsible->username;
-            $url = \yii\helpers\Url::to(['/activity/default/responsible-list']);
-
-            echo $form->field($model, 'next_by')->widget(Select2::classname(), [
-                'initValueText' => $userDesc,
-                'options' => ['placeholder' => 'responsible ...'],
-                'pluginOptions' => [
-                    'allowClear' => true,
-                    'minimumInputLength' => 3,
-                    'ajax' => [
-                        'url' => $url,
-                        'dataType' => 'json',
-                        'data' => new JsExpression('function(params) { return {q:params.term}; }')
-                    ],
-                    'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
-                    'templateResult' => new JsExpression('function(user) { return user.text; }'),
-                    'templateSelection' => new JsExpression('function (user) { return user.text; }'),
-                ],
-            ])->label(false);
-            ?>
+    <div class="form-group" data-activity="form-group">
+            <div class="col-sm-4">
+                <?= \Yii::t('net_frenzel_activity','When'); ?>:
+                <?= DateTimePicker::widget([
+                    'model' => $model,
+                    'attribute' => 'next_at',
+                    'type' => DateTimePicker::TYPE_COMPONENT_PREPEND,
+                    'pluginOptions' => [
+                        'autoclose'=>true,
+                    ]
+                ]); ?>
+            <?= Html::error($model, 'next_at', ['data-activity' => 'form-summary', 'class' => 'help-block hidden']) ?>
         </div>
-        <div class="col-md-3">
-            <?= Html::submitButton('<i class="fa fa-check"></i> ' . \Yii::t('net_frenzel_activity', 'submit'), ['class' => 'btn btn-success btn-block']); ?>
+        <div class="col-sm-8"><?= \Yii::t('net_frenzel_activity','Next'); ?>:
+            <?= $form->field($model, 'next_type')->radioButtonGroup($model->NextTypeArray,[
+                    //'class' => 'btn-group-sm',
+                    'id' => 'next_type-create',
+                    'itemOptions' => ['labelOptions' => ['class' => 'btn btn-primary btn-sm']]
+                ])->label(false);?>
+            <?= Html::error($model, 'next_type', ['data-activity' => 'form-summary', 'class' => 'help-block hidden']) ?>
+        </div>    
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-9">
+                <?php
+                $userDesc = empty($model->next_by) ? '' : $model->responsible->username;
+                $url = \yii\helpers\Url::to(['/activity/default/responsible-list']);
+
+                echo $form->field($model, 'next_by')->widget(Select2::classname(), [
+                    'initValueText' => $userDesc,
+                    'options' => ['placeholder' => 'responsible ...'],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                        'minimumInputLength' => 3,
+                        'ajax' => [
+                            'url' => $url,
+                            'dataType' => 'json',
+                            'data' => new JsExpression('function(params) { return {q:params.term}; }')
+                        ],
+                        'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                        'templateResult' => new JsExpression('function(user) { return user.text; }'),
+                        'templateSelection' => new JsExpression('function (user) { return user.text; }'),
+                    ],
+                ])->label(false);
+                ?>
+            </div>
+            <div class="col-md-3">
+                <?= Html::submitButton('<i class="fa fa-check"></i> ' . \Yii::t('net_frenzel_activity', 'submit'), ['class' => 'btn btn-success btn-block']); ?>
+            </div>
         </div>
     </div>
 </div>
