c3e70896c0cb89c9ce32697003c5aa981d7ee2cb
Philipp Frenzel <philipp@frenzel.net>
2015-10-17 19:07:44 +0200

    add monthly view

---
+++ views/widgets/calendar/MonthWidget.php
@@ -0,0 +1,56 @@
+<?php
+namespace net\frenzel\activity\views\widgets\calendar;
+
+/**
+ * @author Philipp Frenzel <philipp@frenzel.net>
+ */
+
+use net\frenzel\activity\models\Activity;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\base\Widget;
+use yii\helpers\Json;
+
+/**
+ * Activity Class
+ */
+class MonthWidget extends Widget
+{   
+    /**
+     * [$entity description]
+     * @var null
+     */
+    public $entity = NULL;
+
+    /**
+     * [$entity_id description]
+     * @var null
+     */
+    public $entity_id = NULL;
+
+    /**
+     * [$targetUrl description]
+     * @var string
+     */
+    public $targetUrl = '/site/index';
+   
+    /**
+     * the fieldname form the entity
+     * @var string entityTitleField defaults to id
+     */
+    public $entityTitleField = 'id';
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        return $this->render('_month_view', [
+            'targetUrl' => $this->targetUrl,
+            'entity' => $this->entity,
+            'entity_id' => $this->entity_id,
+            'entityTitleField' => $this->entityTitleField,
+        ]);
+    }
+}
